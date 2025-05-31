3604c66c31893e2136cba05ccb001b9256a31a60
seledun <s.ledung@gmail.com>
2025-05-31 14:15:44 +0200

    Update styling and website title

--- src/components/PlayArea.jsx
+++ src/components/PlayArea.jsx
@@ -1,22 +1,28 @@
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { ProgressBar } from "react-bootstrap";
 
 export function PlayArea({ isPlayMode }) {
 
-    return(isPlayMode ? 
+    return (isPlayMode ? (
         <div id="play-area">
             <span id="play-area-question">What</span>
-            <div id="play-area-buttons">
-                <Button variant="primary">Answer 1</Button>
-                <Button variant="primary">Answer 2</Button>
-                <Button variant="primary">Answer 3</Button>
+            <div id="play-area-buttons" className="row gy-0">
+                <div className="col-12 col-lg-6">
+                    <Button variant="primary" className="answer-button w-100 mb-2">Answer 1</Button>
+                    <Button variant="primary" className="answer-button w-100">Answer 2</Button>
+                </div>
+                <div className="col-12 col-lg-6">
+                    <Button variant="primary" className="answer-button w-100 mb-2">Answer 3</Button>
+                    <Button variant="primary" className="answer-button w-100">Answer 4</Button>
+                </div>
             </div>
             <ProgressBar striped variant="success" now={100} />
             <div id="play-area-remaining-time">10 s</div>
         </div>
-        :
+    ) : (
         <div id="history-area">
             History
         </div>
-    );
+    ));
 }
