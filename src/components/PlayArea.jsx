9efbc9fc6268bd0ced0c2ada2ed2cbb8c7259018
seledun <s.ledung@gmail.com>
2025-05-31 11:52:24 +0200

    Create quiz layout

--- src/components/PlayArea.jsx
+++ src/components/PlayArea.jsx
@@ -1,8 +1,18 @@
+import { Button } from "react-bootstrap";
+import { ProgressBar } from "react-bootstrap";
+
 export function PlayArea({ isPlayMode }) {
 
     return(isPlayMode ? 
         <div id="play-area">
-            Play
+            <span id="play-area-question">What</span>
+            <div id="play-area-buttons">
+                <Button variant="primary">Answer 1</Button>
+                <Button variant="primary">Answer 2</Button>
+                <Button variant="primary">Answer 3</Button>
+            </div>
+            <ProgressBar striped variant="success" now={100} />
+            <div id="play-area-remaining-time">10 s</div>
         </div>
         :
         <div id="history-area">
